2581a39909f5a39384ad10192478cf989b22c065
Thomas Helland <thomas.helland97@gmail.com>
2025-09-01 14:44:59 +0200

    Improved registration form and added bugs

--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -1,29 +1,153 @@
+import { useState } from "react";
+
 function Registration() {
+  const [formData, setFormData] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    userName: "",
+    street: "",
+    city: "",
+    zip: "",
+    country: "",
+    phone: "",
+    password: "",
+    role: "user",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const body = {
+      fname: formData.fname,
+      lname: formData.lname,
+      email: formData.email,
+      userName: formData.userName,
+      address: {
+        street: formData.street,
+        city: formData.street,
+        zip: parseInt(formData.zip, 10),
+        country: formData.country,
+      },
+      phone: formData.phone,
+      password: formData.password,
+      role: formData.role,
+    };
+
+    try {
+      const res = await fetch("http://localhost:3000/create_user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+
+      const data = await res.json();
+      console.log("Server response: ", data);
+
+      if (res.ok) {
+        alert("User created successfully!");
+      } else {
+        alert("Error: ", data.error);
+      }
+    } catch (err) {
+      console.log("Fetch error: ", err);
+      alert("Something went wrong!");
+    }
+  };
+
   return (
-    <div className="registration">
-      <form name="registration-form">
-        <label name="reg-fname">First Name</label>
-        <input name="reg-fname" type="text" />
-        <label name="reg-lname">Last Name</label>
-        <input name="reg-lname" type="text" />
-        <label name="reg-email">Email</label>
-        <input name="reg-email" type="email" />
-        <label name="reg-uname"> User name</label>
-        <input name="reg-uname" type="text" />
-        <label name="reg-street">Street</label>
-        <input name="reg-street" type="text" />
-        <label name="reg-city">City</label>
-        <input name="reg-city" type="text" />
-        <label name="reg-zip">Zip code</label>
-        <input name="reg-zip" type="number" />
-        <label name="reg-country">Country</label>
-        <input name="reg-country" type="text" />
-        <label name="reg-phone">Phone number</label>
-        <input name="reg-phone" type="text" />
-        <label name="reg-password">Password</label>
-        <input name="reg-password" type="password" />
-      </form>
-    </div>
+    <form name="registration-form" onSubmit={handleSubmit}>
+      <label name="fname">First Name</label>
+      <input
+        name="fname"
+        placeholder="First name"
+        value={formData.fname}
+        onChange={handleChange}
+      />
+
+      <label name="lname">Last Name</label>
+      <input
+        name="lname"
+        placeholder="Last name"
+        value={formData.lname}
+        onChange={handleChange}
+      />
+
+      <label name="email">Email</label>
+      <input
+        name="email"
+        placeholder="Email"
+        value={formData.email}
+        onChange={handleChange}
+      />
+
+      <label name="userName"> User name</label>
+      <input
+        name="userName"
+        placeholder="User name"
+        value={formData.userName}
+        onChange={handleChange}
+      />
+
+      <label name="street">Street</label>
+      <input
+        name="street"
+        placeholder="Street"
+        value={formData.street}
+        onChange={handleChange}
+      />
+
+      <label name="city">City</label>
+      <input
+        name="city"
+        placeholder="City"
+        value={formData.city}
+        onChange={handleChange}
+      />
+
+      <label name="zip">Zip code</label>
+      <input
+        name="zip"
+        placeholder="Zip code"
+        value={formData.zip}
+        onChange={handleChange}
+      />
+
+      <label name="country">Country</label>
+      <input
+        name="country"
+        placeholder="Country"
+        value={formData.country}
+        onChange={handleChange}
+      />
+
+      <label name="phone">Phone number</label>
+      <input
+        name="phone"
+        placeholder="Phone number"
+        value={formData.phone}
+        onChange={handleChange}
+      />
+
+      <label name="password">Password</label>
+      <input
+        name="password"
+        placeholder="Password"
+        value={formData.password}
+        onChange={handleChange}
+      />
+
+      <button type="submit">Create user</button>
+    </form>
   );
 }
 
