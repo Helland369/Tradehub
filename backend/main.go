7b05eca015f4e5f4c51cb34aea8cc96257d3e4f8
Thomas Helland <thomas_helland@pm.me>
2025-09-03 13:31:01 +0200

    Updated login form

--- backend/main.go
+++ backend/main.go
@@ -43,7 +43,7 @@ app := fiber.New()
 		AllowHeaders: []string{"origin", "Content-Type", "Accept"},
 	}))
 	
-	app.Get("/users", users.GetUser(client))
+	app.Post("/login_users", users.GetUser(client))
 	app.Post("/create_users", users.PostUser(client))
 	// app.Patch("/users/:id", update_user)
 	// app.Delete("/users/:id", delete_users)
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,13 +1,67 @@
+import { useState } from "react";
+
 function Login() {
+  const [formData, setFormData] = useState({
+    userName: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handelSubmit = async (e) => {
+    e.preventDefault();
+
+    const body = {
+      userName: formData.userName,
+      password: formData.password,
+    };
+
+    try {
+      const res = await fetch("http://localhost:3000/get_users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+
+      const data = await res.json();
+      console.log("Server response: ", data);
+
+      if (res.ok) {
+        alert("Login successfully logged in!");
+      } else {
+        alert("Error: ", data.error);
+      }
+    } catch (err) {
+      console.log("Ferch error: ", err);
+      alert("Something went wrong!");
+    }
+  };
+
   return (
-    <div className="login-page">
-      <form className="login-form">
-        <label name="login-email">Login Email</label>
-        <input name="login-email" />
-        <label name="login-password"></label>
-        <input name="login-password" />
-      </form>
-    </div>
+    <form className="login-form" onSubmit={handelSubmit}>
+      <label name="userName">User name</label>
+      <input
+        name="userName"
+        placeholder="User name"
+        value={formData.userName}
+        onChange={handleChange}
+      />
+      <label name="password"></label>
+      <input
+        name="password"
+        placeholder="Password"
+        value={formData.password}
+        onChange={handleChange}
+      />
+      <button type="submit"></button>
+    </form>
   );
 }
 
