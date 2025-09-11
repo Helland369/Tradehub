e5f4b184006e5832d3528c4648cff1bb3003ac21
Thomas Helland <thomas_helland@pm.me>
2025-09-11 12:44:35 +0200

    Added edit user end point to the backend

---
+++ backend/src/users/EditUser.go
@@ -0,0 +1,70 @@
+package users
+
+import (
+	"context"
+
+	"github.com/gofiber/fiber/v3"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func EditUser(client *mongo.Client) fiber.Handler {
+	return func(c fiber.Ctx) error {
+		var body struct {
+			ID string `json:"id"`
+			Fname string `json:"fname"`
+			Lname string `json:"lname"`
+			Email string `json:"email"`
+			UserName string `json:"userName"`
+			Street string `json:"street"`
+			City string `json:"city"`
+			Zip string `json:"zip"`
+			Country string `json:"country"`
+			Phone string `json:"phone"`
+			Password string `json:"Password"`
+		}
+
+		if err := c.Bind().Body(&body); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "cannot parse JSON",
+			})
+		}
+
+		objID, err := primitive.ObjectIDFromHex(body.ID)
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "invalid user ID",
+			})
+		}
+		
+		users := client.Database("tradehub").Collection("users")
+
+		update := bson.M{
+			"$set": bson.M{
+				"ID": body.ID,
+				"Fname": body.Fname,
+				"lname": body.Lname,
+				"email": body.Email,
+				"userName": body.UserName,
+				"street": body.Street,
+				"city": body.City,
+				"zip": body.Zip,
+				"country": body.Country,
+				"phone": body.Phone,
+				"password": body.Password,
+			},
+		}
+		
+		_, err = users.UpdateOne(context.TODO(), bson.M{"_id": objID}, update)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "failed to update user",
+			})
+		}
+
+		return c.JSON(fiber.Map{
+			"message": "user uppdated successfully",
+		})
+	}
+}
