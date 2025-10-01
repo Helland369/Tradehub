ac2cd1d32a416511d0dc33473dc06d27d3e62989
Thomas Helland <thomas_helland@pm.me>
2025-10-01 14:35:23 +0200

    Can now click on a listing and see more info about the listing

--- frontend/src/components/ItemCard.jsx
+++ frontend/src/components/ItemCard.jsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
 function ItemCard({ item }) {
   return (
     <div className="itemCard">
-      <h3>{item.title}</h3>
-      <div className="item-image">
-        <img src={`http://localhost:4000/${item.images}`} alt="No image" />
-      </div>
+      <Link to={`/item/${item._id}`}>
+        <h3>{item.title}</h3>
+        <div className="item-image">
+          <img src={`http://localhost:4000/${item.images}`} alt="No image" />
+        </div>
+      </Link>
       <div className="item-info">
         <p>Description: {item.description}</p>
         <p>Price: {item.buyPrice}</p>
