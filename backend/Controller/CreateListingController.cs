16afd5773fb6ff2af522834ec3a73fce27f64f29
Thomas Helland <thomas_helland@pm.me>
2025-10-29 14:51:09 +0100

    Added create listings controller

---
+++ backend/Controller/CreateListingController.cs
@@ -0,0 +1,59 @@
+using System.Net.Mime;
+using System.Security.Claims;
+using Backend.Models;
+using Backend.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+
+namespace Backend.Controller;
+
+[Route("/listing")]
+[ApiController]
+public class CreateListingController : ControllerBase
+{
+    private readonly TradehubDbContext _db;
+    
+    public CreateListingController(TradehubDbContext db)
+    {
+        _db = db;
+    }
+    
+    [HttpPost]
+    [Authorize]
+    public async Task<ActionResult<IEnumerable<Listing>>> Listing([FromBody]Listing req, CancellationToken ct)
+    {
+        var claimsIdentity = User.FindFirstValue(ClaimTypes.NameIdentifier)
+            ?? throw new UnauthorizedAccessException();
+
+        if (string.IsNullOrWhiteSpace(claimsIdentity) || !ObjectId.TryParse(claimsIdentity, out var userId))
+            return Unauthorized("Invalid or missing user id in token");
+        
+        var listing = new Listing
+        {
+            ID = ObjectId.GenerateNewId(),
+            UserID = userId,
+            Title = req.Title,
+            Description = req.Description,
+            Category = req.Category,
+            Condition = req.Condition,
+            Images = req.Images ?? new(),
+            Status = req.Status,
+            BuyPrice = req.BuyPrice,
+            CurrentBid = req.CurrentBid,
+            SellerID = req.SellerID,
+            Bids = req.Bids,
+            IsAuction = req.IsAuction || true,
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = req.UpdatedAt,
+            EndTime = req.EndTime,
+            // need status...
+            Watchers = req.Watchers ?? new(),
+            Location = req.Location,
+        };
+
+        _db.Listings.AddAsync(listing);
+        _db.SaveChangesAsync(ct);
+        return Ok(listing);
+    }
+}
