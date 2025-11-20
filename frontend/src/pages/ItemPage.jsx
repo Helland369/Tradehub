b61428cbc562cd9377ccfe2fef1271d64182232f
Thomas Helland <thomas_helland@pm.me>
2025-11-20 11:02:29 +0100

    Added add to cart button

--- frontend/src/pages/ItemPage.jsx
+++ frontend/src/pages/ItemPage.jsx
@@ -4,6 +4,9 @@ import { useEffect, useState } from "react";
 function ItemPage() {
   const { id } = useParams();
   const [item, setItem] = useState(null);
+  const [message, setMessage] = useState("");
+
+  const token = localStorage.getItem("token");
 
   useEffect(() => {
     async function fetchItem() {
@@ -35,13 +38,48 @@ function ItemPage() {
     });
   };
 
+  const addToCart = async () => {
+    if (!token) {
+      setMessage("You must be logged inn to add to cart!");
+    }
+
+    try {
+      const res = await fetch("http://localhost:3000/api/add_to_cart", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(item.id),
+      });
+
+      let data = null;
+
+      try {
+        data = await res.json();
+      } catch (err) {
+        setMessage("There was no json!");
+      }
+
+      if (res.ok) {
+        setMessage("Successfully added item to cart!");
+      } else {
+        setMessage(`Error: ${data.error || "Unknown error"}`);
+      }
+    } catch (err) {
+      console.log(err);
+      setMessage("Something went wrong when adding to cart!");
+    }
+  };
+
   return (
     <div>
       <h1>{item.title}</h1>
       <div>{renderImage()}</div>
       <p>{item.description}</p>
       <p>Price: {item.buyPrice}</p>
-      <button>Buy</button>
+      <button onClick={addToCart}>Add to cart</button>
+      {message && <p>{message}</p>}
     </div>
   );
 }
