15b95bf81679ebaebc494558f262896472f9518b
Thomas Helland <thomas_helland@pm.me>
2026-04-07 17:05:17 +0200

    Fixed issues with displaying images on home page

--- frontend/src/components/ItemCard.jsx
+++ frontend/src/components/ItemCard.jsx
@@ -3,20 +3,27 @@ import { Link } from "react-router-dom";
 function ItemCard({ item }) {
   const imgPath = Array.isArray(item.images) ? item.images[0] : item.images;
 
-  const baseUrl = "http://localhost:3000";
+  let src = undefined;
 
-  const formatedPath = imgPath?.startsWith("uploads/listings/")
-    ? imgPath
-    : imgPath?.replace("uploads/", "uploads/listings/");
+  if (imgPath && typeof imgPath === "string") {
+    const path =
+      imgPath.startsWith("uploads/") && !imgPath.includes("uploads/listings/")
+        ? imgPath.replace("uploads/", "uploads/listings/")
+        : imgPath;
 
-  const src = imgPath ? `${baseUrl}${formatedPath}` : undefined;
+    src = `http://localhost:3000/${path}`;
+  }
 
   return (
     <div className="itemCard">
       <Link to={`/item/${item.id}`}>
         <h3>{item.title}</h3>
         <div className="item-image">
-          <img src={src} alt={item.title} />
+          {src ? (
+            <img src={src} alt={item.title} />
+          ) : (
+            <div className="placeholder">No Image</div>
+          )}
         </div>
       </Link>
       <div className="item-info">
