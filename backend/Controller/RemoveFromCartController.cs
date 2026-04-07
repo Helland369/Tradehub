af6ca73cdd6643561b4de6cf259bdadb2964902d
Thomas Helland <thomas_helland@pm.me>
2026-04-07 17:42:13 +0200

    Added try catch to RemoveFromCartController

--- backend/Controller/RemoveFromCartController.cs
+++ backend/Controller/RemoveFromCartController.cs
@@ -18,37 +18,46 @@ public class RemoveFromCartController : ControllerBase
         _db = db;
         _uservice = uservice;
     }
-    
+
     [HttpDelete]
     [Authorize]
     public async Task<IActionResult> RemoveFromCart([FromBody] RemoveFromCart dto, CancellationToken ct)
     {
-        if (!_uservice.TryGetUserId(out var userId))
-            return Unauthorized("Invalid or missing user id in token");
-
-        if (!ObjectId.TryParse(dto.ItemId, out var itmeId))
-            return BadRequest("Invalid item id");
-        
-        var user = _db.Users.FirstOrDefault(u => u.ID == userId);
-        if (user == null)
-            return BadRequest("User not found");
-
-        var cartItem = user.Cart.FirstOrDefault(c => c.ListingID == itmeId);
-        if (cartItem == null)
-            return NotFound("Cart item not found");
-        
-        var removeQty = dto.Quantity <=  0 ? 1 : dto.Quantity;
-
-        if (cartItem.Quantity > removeQty)
+        try
         {
-            cartItem.Quantity -= removeQty;
+            if (!_uservice.TryGetUserId(out var userId))
+                return Unauthorized("Invalid or missing user id in token");
+
+            if (!ObjectId.TryParse(dto.ItemId, out var itmeId))
+                return BadRequest("Invalid item id");
+
+            var user = _db.Users.FirstOrDefault(u => u.ID == userId);
+            if (user == null)
+                return BadRequest("User not found");
+
+            var cartItem = user.Cart.FirstOrDefault(c => c.ListingID == itmeId);
+            if (cartItem == null)
+                return NotFound("Cart item not found");
+
+            var removeQty = dto.Quantity <= 0 ? 1 : dto.Quantity;
+
+            if (cartItem.Quantity > removeQty)
+            {
+                cartItem.Quantity -= removeQty;
+            }
+            else
+            {
+                _ = user.Cart.Remove(cartItem);
+            }
+
+            _ = await _db.SaveChangesAsync(ct);
+            return Ok();
+
         }
-        else
+        catch (Exception ex)
         {
-            user.Cart.Remove(cartItem);
+            Console.WriteLine(ex);
+            return BadRequest();
         }
-        
-        await _db.SaveChangesAsync(ct);
-        return Ok();
     }
 }
